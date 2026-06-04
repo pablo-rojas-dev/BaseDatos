d6ca05ef5c8bac01fb2a9ad1501d8149c8f3855f
Pablo Rojas <123708990+pablo-rojas-dev@users.noreply.github.com>
2026-06-03 20:45:04 -0600

    Ejercicio 3

---
+++ app/src/main/java/com/example/basedatos/converters/Converters.kt
@@ -0,0 +1,12 @@
+package com.example.basedatos.converters
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromDate(date: Date): Long = date.time
+    @TypeConverter
+    fun toDate(timestamp: Long): Date =
+        Date(timestamp)
+}
